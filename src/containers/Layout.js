5719ede50d126a5fc4958a076c1512f2207d7f42
Mwinami <mwinamijr@gmail.com>
2021-12-12 05:57:49 +0300

    modified the general layout

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -30,7 +30,7 @@ class DashboardLayout extends React.Component {
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1" icon={<PieChartOutlined />}>
-              <Link to="/">Dashboard</Link>
+              <Link to="/dashboard">Dashboard</Link>
             </Menu.Item>
             <Menu.Item key="2" icon={<DesktopOutlined />}>
               Option 2
@@ -41,25 +41,22 @@ class DashboardLayout extends React.Component {
               <Menu.Item key="5">Alex</Menu.Item>
             </SubMenu>
             <SubMenu key="sub2" icon={<TeamOutlined />} title="Finance">
-              <Menu.Item key="6">Receipts</Menu.Item>
-              <Menu.Item key="8">Payments</Menu.Item>
+              <Menu.Item key="6"><Link to="/receipts">Receipts</Link></Menu.Item>
+              <Menu.Item key="8"><Link to="/payments">Payments</Link></Menu.Item>
+              <Menu.Item key="11"><Link to="/reports">Reports</Link></Menu.Item>
             </SubMenu>
             <Menu.Item key="9" icon={<FileOutlined />}>
               Files
             </Menu.Item>
             <Menu.Item key="10" icon={<FileOutlined />}>
-              <Link to="/login">Login</Link>
+              <Link to="/logout">Logout</Link>
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <div className="site-layout-background" style={{ padding: 0, minHeight: 360 }}>
             { this.props.children }
             </div>
           </Content>
