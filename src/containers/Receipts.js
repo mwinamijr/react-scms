b492f539a20854d9559aa9767e6aced7795abc72
Mwinami <mwinamijr@gmail.com>
2021-12-13 17:27:07 +0300

    modified the links for receipts list and new receipts tabs

--- src/containers/Receipts.js
+++ src/containers/Receipts.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Breadcrumb } from 'react-bootstrap';
-
+import React, {useState} from 'react'
+import { Breadcrumb, Table, Tabs, Tab } from 'react-bootstrap';
 
 function Receipts() {
+  const [key, setKey] = useState('receipts')
     return (
       <div>
         <Breadcrumb>
@@ -12,11 +12,66 @@ function Receipts() {
           </Breadcrumb.Item>
           <Breadcrumb.Item active>Data</Breadcrumb.Item>
         </Breadcrumb>
-        <div>
-        Receipts
-        
+      <div>
+      <div>
+        <div className="text-center">
+          <h1>Receipts</h1>
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+            className="mb-3"
+          >
+            <Tab eventKey="receipts" title="Receipts">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Receipt No</th>
+                    <th>Allocation</th>
+                    <th>Paid for</th>
+                    <th>Amonunt</th>
+                    <th>Received by</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                    <th>Username</th>
+                  </tr>
+                </thead>
+              </Table>
+            </Tab>
+            <Tab eventKey="new" title="New">
+              New receipt
+            </Tab>
+          </Tabs>
         </div>
-
+      </div>
+      </div>
       </div>
     )
 }
