54830c74176e8078ae457ddfd27cd4d579930665
Mwinami <mwinamijr@gmail.com>
2021-12-26 00:01:11 +0300

    added routes to show acording to user authenticated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import {React} from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './containers/Home';
 import Login from './containers/Login';
-
 import DashboardLayout from './containers/Layout';
 
+import { logout } from './actions/userActions'
 import BaseRouter from "./routes";
 
 function App(props) {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
 
   return (
 	<div className="App">
 		<Router>
-			{ isAuthenticated ? 
+			{ userInfo ? 
 				<DashboardLayout> 
 					<BaseRouter />
 				</DashboardLayout>
