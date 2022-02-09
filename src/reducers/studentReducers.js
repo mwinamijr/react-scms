93d1e079713d5809e178976b82c6706a373408dd
mwinamijr <mwinamijr@gmail.com>
2022-02-09 18:57:26 +0300

    created student details reducer

--- src/reducers/studentReducers.js
+++ src/reducers/studentReducers.js
@@ -1,6 +1,6 @@
 import {
-    STUDENT_LIST_REQUEST, STUDENT_LIST_SUCCESS, STUDENT_LIST_FAIL, /*STUDENT_LIST_RESET,
-    STUDENT_DETAIL_START, STUDENT_DETAILS_SUCCESS, STUDENT_DETAILS_FAIL, STUDENT_DETAILS_RESET,
+    STUDENT_LIST_REQUEST, STUDENT_LIST_SUCCESS, STUDENT_LIST_FAIL,
+    STUDENT_DETAILS_REQUEST, STUDENT_DETAILS_SUCCESS, STUDENT_DETAILS_FAIL, /* STUDENT_LIST_RESET, STUDENT_DETAILS_RESET,
     STUDENT_CREATE_REQUEST, STUDENT_CREATE_SUCCESS, STUDENT_CREATE_FAIL, STUDENT_CREATE_RESET,
     STUDENT_UPDATE_REQUEST, STUDENT_UPDATE_SUCCESS, STUDENT_UPDATE_FAIL, STUDENT_UPDATE_RESET, 
     STUDENT_DELETE_REQUEST, STUDENT_DELETE_SUCCESS, STUDENT_DELETE_FAIL*/
@@ -18,6 +18,22 @@ export const studentListReducer = (state = { students: [] }, action) => {
             }
 
         case STUDENT_LIST_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const studentDetailsReducer = (state = { student: [] }, action) => {
+    switch (action.type) {
+        case STUDENT_DETAILS_REQUEST:
+            return { loading: true, ...state }
+
+        case STUDENT_DETAILS_SUCCESS:
+            return { loading: false, student: action.payload }
+
+        case STUDENT_DETAILS_FAIL:
             return { loading: false, error: action.payload }
 
         default:
