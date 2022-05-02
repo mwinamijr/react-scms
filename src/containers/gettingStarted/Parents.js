ab41aeb5d5b7a06ea835fa0488d9e4f1f34a6eb0
mwinamijr <mwinamijr@gmail.com>
2022-05-02 17:49:11 +0300

    created getting started pages for parents, taechers and developers

---
+++ src/containers/gettingStarted/Parents.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Parents() {
+  return (
+    <div>
+      <Link to="/" className='btn btn-light my-3'>Go Back</Link>
+      Parents getting started
+    </div>
+  )
+}
+
+export default Parents
---
+++ src/containers/gettingStarted/Teachers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Teachers() {
+  return (
+    <div>
+      <Link to="/" className='btn btn-light my-3'>Go Back</Link>
+      Teachers getting started
+    </div>
+  )
+}
+
+export default Teachers
