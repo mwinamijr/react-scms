a6e797778dbd730f4cc406769eb6ef13303abf09
mwinamijr <mwinamijr@gmail.com>
2022-05-05 15:31:28 +0300

    changed layout to react functional component

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import {
@@ -12,22 +12,20 @@ import {
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-class DashboardLayout extends React.Component {
-  state = {
-    collapsed: false,
-  };
 
-  onCollapse = collapsed => {
+function Layout2(props) {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const onCollapse = collapsed => {
     console.log(collapsed);
-    this.setState({ collapsed });
+    setCollapsed(!collapsed);
   };
 
-  render() {
-    const { collapsed } = this.state;
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
           <div className="logo" />
+          
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1" icon={<PieChartOutlined />}>
               <Link to="/">Dashboard</Link>
@@ -67,14 +65,13 @@ class DashboardLayout extends React.Component {
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
             <div className="site-layout-background" style={{ padding: 0, minHeight: 360 }}>
-            { this.props.children }
+            { props.children }
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>Hayatul Islamiya ©2022 Created by Techdometz</Footer>
         </Layout>
       </Layout>
-    );
-  }
+  )
 }
 
-export default DashboardLayout;
+export default Layout2
