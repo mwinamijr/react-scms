83eab9d80edca993886e44c2f8554bffeecbf8b9
mwinamijr <mwinamijr@gmail.com>
2022-05-05 22:19:52 +0300

    fixed conditional login for different users

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -21,7 +21,7 @@ function DashLayout(props) {
 
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
-
+  console.log("isTeacher:",userInfo.user_type.isAccountant)
   const dispatch = useDispatch()
 
   const onCollapse = collapsed => {
@@ -37,41 +37,89 @@ function DashLayout(props) {
     <Layout style={{ minHeight: '100vh' }}>
         <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
           <div className="logo" />
-          
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              <Link to="/">Dashboard</Link>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Admission
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="Employees">
-              <Menu.Item key="3"><Link to="/teachers">Teachers</Link></Menu.Item>
-              <Menu.Item key="4">Accountants</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Students">
-              <Menu.Item key="6"><Link to="/sis/students">Students</Link></Menu.Item>
-              <Menu.Item key="7"><Link to="/exams">Exams</Link></Menu.Item>
-              <Menu.Item key="8"><Link to="/results">Results</Link></Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" icon={<TeamOutlined />} title="Finance">
-              <Menu.Item key="9"><Link to="/receipts">Receipts</Link></Menu.Item>
-              <Menu.Item key="10"><Link to="/payments">Payments</Link></Menu.Item>
-              <Menu.Item key="11"><Link to="/reports">Reports</Link></Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub4" icon={<TeamOutlined />} title="Learn">
-              <Menu.Item key="12"><Link to="/learn/assignments">Assignment</Link></Menu.Item>
-              <Menu.Item key="13"><Link to="/exams">Exams</Link></Menu.Item>
-              <Menu.Item key="14"><Link to="/results">Results</Link></Menu.Item>
-            </SubMenu>
-            <Menu.Item key="15" icon={<FileOutlined />}>
-              Files
-            </Menu.Item>
-            <Menu.Item key="16" icon={<FileOutlined />}>
-              <Link onClick={logoutHandler} to='/'>Logout</Link>
-            </Menu.Item>
-          </Menu>
+          { userInfo.user_type.isTeacher ?
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Link to="/">Dashboard</Link>
+              </Menu.Item>
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="Students">
+                <Menu.Item key="6"><Link to="/sis/students">Students</Link></Menu.Item>
+                <Menu.Item key="7"><Link to="/exams">Exams</Link></Menu.Item>
+                <Menu.Item key="8"><Link to="/results">Results</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" icon={<TeamOutlined />} title="Learn">
+                <Menu.Item key="12"><Link to="/learn/assignments">Assignment</Link></Menu.Item>
+                <Menu.Item key="13"><Link to="/exams">Exams</Link></Menu.Item>
+                <Menu.Item key="14"><Link to="/results">Results</Link></Menu.Item>
+              </SubMenu>
+              <Menu.Item key="15" icon={<FileOutlined />}>
+                Files
+              </Menu.Item>
+              <Menu.Item key="16" icon={<FileOutlined />}>
+                <Link onClick={logoutHandler} to='/'>Logout</Link>
+              </Menu.Item>
+            </Menu>
+            : userInfo.user_type.isAccountant ?
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Link to="/">Dashboard</Link>
+              </Menu.Item>
+              <Menu.Item key="2" icon={<DesktopOutlined />}>
+                Admission
+              </Menu.Item>
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="Students">
+                <Menu.Item key="6"><Link to="/sis/students">Students</Link></Menu.Item>
+                <Menu.Item key="7"><Link to="/exams">Exams</Link></Menu.Item>
+                <Menu.Item key="8"><Link to="/results">Results</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<TeamOutlined />} title="Finance">
+                <Menu.Item key="9"><Link to="/receipts">Receipts</Link></Menu.Item>
+                <Menu.Item key="10"><Link to="/payments">Payments</Link></Menu.Item>
+                <Menu.Item key="11"><Link to="/reports">Reports</Link></Menu.Item>
+              </SubMenu>
+              <Menu.Item key="15" icon={<FileOutlined />}>
+                Files
+              </Menu.Item>
+              <Menu.Item key="16" icon={<FileOutlined />}>
+                <Link onClick={logoutHandler} to='/'>Logout</Link>
+              </Menu.Item>
+            </Menu>
+            :
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Link to="/">Dashboard</Link>
+              </Menu.Item>
+              <Menu.Item key="2" icon={<DesktopOutlined />}>
+                Admission
+              </Menu.Item>
+              <SubMenu key="sub1" icon={<UserOutlined />} title="Employees">
+                <Menu.Item key="3"><Link to="/teachers">Teachers</Link></Menu.Item>
+                <Menu.Item key="4">Accountants</Menu.Item>
+                <Menu.Item key="5">Alex</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="Students">
+                <Menu.Item key="6"><Link to="/sis/students">Students</Link></Menu.Item>
+                <Menu.Item key="7"><Link to="/exams">Exams</Link></Menu.Item>
+                <Menu.Item key="8"><Link to="/results">Results</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<TeamOutlined />} title="Finance">
+                <Menu.Item key="9"><Link to="/receipts">Receipts</Link></Menu.Item>
+                <Menu.Item key="10"><Link to="/payments">Payments</Link></Menu.Item>
+                <Menu.Item key="11"><Link to="/reports">Reports</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" icon={<TeamOutlined />} title="Learn">
+                <Menu.Item key="12"><Link to="/learn/assignments">Assignment</Link></Menu.Item>
+                <Menu.Item key="13"><Link to="/exams">Exams</Link></Menu.Item>
+                <Menu.Item key="14"><Link to="/results">Results</Link></Menu.Item>
+              </SubMenu>
+              <Menu.Item key="15" icon={<FileOutlined />}>
+                Files
+              </Menu.Item>
+              <Menu.Item key="16" icon={<FileOutlined />}>
+                <Link onClick={logoutHandler} to='/'>Logout</Link>
+              </Menu.Item>
+            </Menu>
+          }
         </Sider>
         <Layout className="site-layout">
           <Header className="site-layout-background" style={{ padding: 0 }} />
