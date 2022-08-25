5d9cc9b75ee25c38bbcc57dad8bd401320ce85cd
mwinamijr <mwinamijr@gmail.com>
2022-08-25 23:55:20 +0300

    change app to use react 18

--- src/index.js
+++ src/index.js
@@ -9,14 +9,15 @@ import store from './store'
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-);
-
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
