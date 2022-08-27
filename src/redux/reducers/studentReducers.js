c2fea4308222282430d9e462a19203a418e26655
mwinamijr <mwinamijr@gmail.com>
2022-08-27 23:35:48 +0300

    created students bulk upload reducer

--- src/redux/reducers/studentReducers.js
+++ src/redux/reducers/studentReducers.js
@@ -2,6 +2,7 @@ import {
     STUDENT_LIST_REQUEST, STUDENT_LIST_SUCCESS, STUDENT_LIST_FAIL,
     STUDENT_DETAILS_REQUEST, STUDENT_DETAILS_SUCCESS, STUDENT_DETAILS_FAIL, 
     STUDENT_CREATE_REQUEST, STUDENT_CREATE_SUCCESS, STUDENT_CREATE_FAIL, STUDENT_CREATE_RESET,
+    STUDENT_BULK_CREATE_REQUEST, STUDENT_BULK_CREATE_SUCCESS, STUDENT_BULK_CREATE_FAIL, 
     /* STUDENT_LIST_RESET, STUDENT_DETAILS_RESET, 
     STUDENT_UPDATE_REQUEST, STUDENT_UPDATE_SUCCESS, STUDENT_UPDATE_FAIL, STUDENT_UPDATE_RESET, 
     STUDENT_DELETE_REQUEST, STUDENT_DELETE_SUCCESS, STUDENT_DELETE_FAIL*/
@@ -55,6 +56,25 @@ export const studentCreateReducer = (state = {}, action) => {
             return { loading: false, error: action.payload }
 
         case STUDENT_CREATE_RESET:
+            return {}
+
+        default:
+            return state
+    }
+}
+
+export const studentsBulkCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case STUDENT_BULK_CREATE_REQUEST:
+            return { loading: true }
+
+        case STUDENT_BULK_CREATE_SUCCESS:
+            return { loading: false, success: true, student: action.payload }
+
+        case STUDENT_BULK_CREATE_FAIL:
+            return { loading: false, error: action.payload }
+
+        case STUDENT_BULK_CREATE_RESET:
             return {}
 
         default:
