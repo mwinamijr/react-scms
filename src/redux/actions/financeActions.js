27d18b43b397d85ff49b1821787426d53e1ee548
mwinamijr <mwinamijr@gmail.com>
2022-10-06 17:56:53 +0300

    created receipt details action

--- src/redux/actions/financeActions.js
+++ src/redux/actions/financeActions.js
@@ -35,3 +35,35 @@ export const listReceipts = () => async (dispatch, getState) => {
         })
     }
 }
+
+export const ReceiptsDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: RECEIPT_DETAILS_REQUEST })
+
+        const {
+            userLogin: { userInfo},
+        } = getState()
+
+        const config = {
+            headers : {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`http://127.0.0.1:8000/api/finance/receipts/${id}`, config)
+
+        dispatch({
+            type: RECEIPT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: RECEIPT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
