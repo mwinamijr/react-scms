baf0871c2ee9325d3cf92ca45f3882ded1f65ac3
mwinamijr <mwinamijr@gmail.com>
2022-10-07 16:11:50 +0300

    created payments list reducer

--- src/redux/reducers/financeReducers.js
+++ src/redux/reducers/financeReducers.js
@@ -1,6 +1,7 @@
 import {
     RECEIPT_LIST_REQUEST, RECEIPT_LIST_SUCCESS, RECEIPT_LIST_FAIL,
     RECEIPT_DETAILS_REQUEST, RECEIPT_DETAILS_SUCCESS, RECEIPT_DETAILS_FAIL,
+    PAYMENT_LIST_REQUEST, PAYMENT_LIST_SUCCESS, PAYMENT_LIST_FAIL,
 } from '../constants/financeConstants'
 
 export const receiptListReducer = (state = { receipts: [] }, action) => {
@@ -31,6 +32,25 @@ export const receiptDetailsReducer = (state = { receipt: [] }, action) => {
             return { loading: false, receipt: action.payload }
 
         case RECEIPT_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const paymentListReducer = (state = { payments: [] }, action) => {
+    switch (action.type) {
+        case PAYMENT_LIST_REQUEST:
+            return { loading: true, payments: [] }
+
+        case PAYMENT_LIST_SUCCESS:
+            return {
+                loading: false,
+                payments: action.payload,
+            }
+
+        case PAYMENT_LIST_FAIL:
             return { loading: false, error: action.payload }
 
         default:
