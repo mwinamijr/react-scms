26cdbfc58a8efd31fafb047bf51043e7bd62a41b
mwinamijr <mwinamijr@gmail.com>
2022-10-07 16:12:07 +0300

    created payment list action

--- src/redux/actions/financeActions.js
+++ src/redux/actions/financeActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
     RECEIPT_LIST_REQUEST, RECEIPT_LIST_SUCCESS, RECEIPT_LIST_FAIL, 
     RECEIPT_DETAILS_REQUEST, RECEIPT_DETAILS_SUCCESS, RECEIPT_DETAILS_FAIL, 
+    PAYMENT_LIST_REQUEST, PAYMENT_LIST_SUCCESS, PAYMENT_LIST_FAIL,
 } from '../constants/financeConstants'
 
 export const listReceipts = () => async (dispatch, getState) => {
@@ -61,6 +62,38 @@ export const ReceiptDetails = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: RECEIPT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
+
+export const listPayments = () => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PAYMENT_LIST_REQUEST })
+        
+        const {
+            userLogin: { userInfo},
+        } = getState()
+
+        const config = {
+            headers : {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get('http://127.0.0.1:8000/api/finance/payments/', config)
+
+        dispatch({
+            type: PAYMENT_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PAYMENT_LIST_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
