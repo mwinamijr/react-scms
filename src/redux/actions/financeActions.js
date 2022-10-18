765050e9a7cb8769c599255e587e3f3136379cd3
mwinamijr <mwinamijr@gmail.com>
2022-10-18 12:18:49 +0300

    created add receipt action

--- src/redux/actions/financeActions.js
+++ src/redux/actions/financeActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
     RECEIPT_LIST_REQUEST, RECEIPT_LIST_SUCCESS, RECEIPT_LIST_FAIL, 
     RECEIPT_DETAILS_REQUEST, RECEIPT_DETAILS_SUCCESS, RECEIPT_DETAILS_FAIL, 
+    RECEIPT_CREATE_REQUEST, RECEIPT_CREATE_SUCCESS, RECEIPT_CREATE_FAIL, RECEIPT_CREATE_RESET,
     PAYMENT_LIST_REQUEST, PAYMENT_LIST_SUCCESS, PAYMENT_LIST_FAIL,
     PAYMENT_DETAILS_REQUEST, PAYMENT_DETAILS_SUCCESS, PAYMENT_DETAILS_FAIL,
 } from '../constants/financeConstants'
@@ -63,6 +64,46 @@ export const ReceiptDetails = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: RECEIPT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
+
+export const createReceipt = ( ) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: RECEIPT_CREATE_REQUEST
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post(
+        'http://127.0.0.1:8000/api/finance/receipts/',{
+        
+        },
+        config
+        )
+
+        dispatch({
+            type: RECEIPT_CREATE_SUCCESS,
+            payload: data,
+        })
+
+
+    } catch (error) {
+        dispatch({
+            type: RECEIPT_CREATE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
