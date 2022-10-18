d10fae3d653f307a43a5102028a909d2726636d0
mwinamijr <mwinamijr@gmail.com>
2022-10-18 12:17:26 +0300

    modified addReceipt page contents

--- src/containers/finance/AddReceipt.js
+++ src/containers/finance/AddReceipt.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Card, Col, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux'
+import React, {useEffect, useState} from 'react'
+import { Card, Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import {
-  Form,
-  Input,
-  Button,
-  Select,
-  DatePicker,
-  InputNumber,
-} from 'antd';
 import Message from '../../components/Message';
 
 function AddReceipt() {
+  const [receiptNumber, setReceiptNumber] = useState('')
+  const [student, setStudent] = useState('')
+  const [payer, setPayer] = useState('')
+  const [paidFor, setPaidFor] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [receivedBy, setReceivedBy] = useState('')
+
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
+
   return (
     <div>
       <Link to="/finance/receipts/" className='btn btn-light my-3'>Go Back</Link>
@@ -32,38 +32,77 @@ function AddReceipt() {
           <Card.Body className="text-left col-md-8">
             <Card.Title className='pb-3'>PAYMENT RECEIPT</Card.Title>
             <Form>
-              <Row>
-                <Col>
-                  <Form.Item label="Date">
-                    <DatePicker />
-                  </Form.Item>
-                </Col>
-                <Col>
-                  <Form.Item label="Receipt Number">
-                    <InputNumber />
-                  </Form.Item>
-                </Col>
-              </Row>
+              <Form.Group>
+                <Form.Label>Receipt Number</Form.Label>
+                <Form.Control 
+                  id="receiptNumber" 
+                  placeholder="Receipt Number" 
+                  required
+                  type='number'
+                  value={receiptNumber}
+                  onChange={(e) =>setReceiptNumber(e.target.value)}
+                />
+              </Form.Group>
               
-              <Form.Item label="Received From">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Amount">
-                <InputNumber />
-              </Form.Item>
-              <Form.Item label="Paid for">
-                <Select>
-                  <Select.Option value="School Fees">School Fees</Select.Option>
-                  <Select.Option value="Exam Fees">Exam Fees</Select.Option>
-                  <Select.Option value="Allowances">Allowances</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item label="Received By">
-                <Input />
-              </Form.Item>
-              <Form.Item>
+              <Form.Group>
+                <Form.Label>Payer</Form.Label>
+                <Form.Control 
+                  id="payer" 
+                  placeholder="payer" 
+                  required
+                  type='text'
+                  value={payer}
+                  onChange={(e) =>setPayer(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Student</Form.Label>
+                <Form.Control 
+                  id="student" 
+                  placeholder="student" 
+                  required
+                  type='text'
+                  value={student}
+                  onChange={(e) =>setStudent(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Select label="Paid For" 
+                id='paidFor'
+                type='text'
+                value={paidFor}
+                onChange={(e) => setPaidFor(e.target.value)}
+              
+              >
+                <option>Paid For</option>
+                <option value="1">School Fees</option>
+                <option value="2">Tour</option>
+                <option value="3">Allowances</option>
+              </Form.Select>
+              <Form.Group>
+                <Form.Label>Amount</Form.Label>
+                <Form.Control 
+                  id="amount" 
+                  placeholder="Amount" 
+                  required
+                  type='number'
+                  value={amount}
+                  onChange={(e) =>setAmount(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Received By</Form.Label>
+                <Form.Control 
+                  id="receivedBy" 
+                  placeholder="receivedBy" 
+                  required
+                  type='text'
+                  value={receivedBy}
+                  onChange={(e) =>setReceivedBy(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group>
                 <Button className='primary'>Submit Payment</Button>
-              </Form.Item>
+              </Form.Group>
             </Form>
           </Card.Body>
         </Card>
