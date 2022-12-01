449b77603bf1b6e2620f93a24bb7a9f255f15fe9
mwinamijr <mwinamijr@gmail.com>
2022-12-01 18:07:59 +0300

    changed contents of the user list page

--- src/containers/users/UserList.js
+++ src/containers/users/UserList.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { Breadcrumb, Table, Row, Col } from 'react-bootstrap';
+import { EditOutlined } from '@ant-design/icons';
+
+import { listStudents } from '../../redux/actions/studentActions';
+import Loader from './../../components/Loader';
+import Message from './../../components/Message';
 
 function UserList() {
-  return (
-    <div>UserList</div>
-  )
+  
+  const dispatch = useDispatch()
+
+  const studentList = useSelector(state => state.studentList)
+  const { loading, error, students } = studentList
+
+  useEffect(() => {
+    dispatch(listStudents())
+    
+}, [dispatch,])
+
+
+    return (
+      <div>
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            <Link to="/">Home</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Users</Breadcrumb.Item>
+        </Breadcrumb>
+      <div>
+      <div>
+        <div>
+          <h1 className="text-center">Users</h1>
+          <Row>
+            <Col><Link to="/sis/students/add" className='btn btn-light my-3'>Add User</Link></Col>
+          </Row>
+            { loading ? <Loader /> :
+              error ? <Message variant="danger">{error}</Message> :
+              
+              <Table striped bordered hover>
+                <thead>
+                  
+                  <tr>
+                    <th>Adm No:</th>
+                    <th>Full Name</th>
+                    <th>Sex</th>
+                    <th>Class</th>
+                    <th>Birthday</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                  
+                
+                <tbody>
+                    
+                  { students.map(student => (
+                    <tr key={student.addmission_number}>
+                      <td>{student.addmission_number}</td>
+                      <td>{student.first_name} {student.middle_name} {student.last_name}</td>
+                      <td>{student.sex}</td>
+                      <td>{student.class_level}</td>
+                      <td>{student.birthday}</td>
+                      <td><Link to={`/sis/students/${student.addmission_number}`}><EditOutlined /></Link></td>
+                    </tr>
+                  ))}
+                
+                </tbody>
+              </Table>
+            }
+        </div>
+      </div>
+      </div>
+      </div>
+    )
 }
 
 export default UserList
