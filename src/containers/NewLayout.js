5e5ff21ec5f94b1b75e23f38f57a5584ff03d64c
mwinamijr <mwinamijr@gmail.com>
2022-12-24 00:27:02 +0300

    added new layout sidebar navigations

--- src/containers/NewLayout.js
+++ src/containers/NewLayout.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import {
-  DesktopOutlined, FileOutlined, PieChartOutlined,
-  TeamOutlined, UserOutlined, AppstoreOutlined
+  DesktopOutlined, MailOutlined, PieChartOutlined, CalculatorOutlined, UserDeleteOutlined,
+  TeamOutlined, UserOutlined, FileExcelOutlined, FileDoneOutlined, CalendarOutlined
 } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
@@ -28,16 +28,36 @@ const items = [
     getItem(<Link to="/teachers">Teachers</Link>, '4'),
     getItem('Accountants', '5'),
   ]),
-  getItem('Finance', 'sub2', <TeamOutlined />, [
+  getItem('Finance', 'sub2', <CalculatorOutlined />, [
     getItem(<Link to="/finance/receipts">Receipts</Link>, '6'), 
-    getItem(<Link to="/finance/payments">Payments</Link>, '8')
+    getItem(<Link to="/finance/payments">Payments</Link>, '24'),
+    getItem(<Link to="/finance/payments">Payroll</Link>, '25'),
+    getItem('Reports', 'sub9', null, [
+      getItem('Collections', '26'),
+      getItem('Invoices', '27')
+    ]),
   ]),
-  getItem('Navigation Two', 'sub3', <AppstoreOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
+  getItem('Exam', 'sub5', <FileDoneOutlined />, [
+    getItem('Setting', 'sub6', null, [getItem('Exam groups', '15'), getItem('School exams', '16')]),
+    getItem('Reports', 'sub7', null, [
+      getItem('Single Report', '17'), 
+      getItem('Combined reports', '18'),
+      getItem('CA Report', '20'),
+    ]),
+    getItem('Exam Schedule', '19'),
+  ]),
+  getItem('Attendance', 'sub8', <FileExcelOutlined />, [
+    getItem('Student Attendance', '21'),
+    getItem('Employee Attendance', '22'),
+    getItem('Attendance report', '23'),
+  ]),
+  getItem('Email / SMS', 'sub3', <MailOutlined />, [
+    getItem('Inbox', '9'),
+    getItem('Compose', '10'),
     getItem('Submenu', 'sub4', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
   ]),
-  getItem('Logout', '13', <FileOutlined />),
+  getItem('School calender', '28', <CalendarOutlined />),
+  getItem('Logout', '13', <UserDeleteOutlined />),
 ];
 const DashLayout = (props) => {
   const [collapsed, setCollapsed] = useState(false);
