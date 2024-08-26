5f6c7e06a044d14f4f62dcff1e3a0013b43b4417
mwinamijr <mwinamijr@gmail.com>
2024-08-26 21:39:30 +0300

    modified homepage links

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,40 +10,43 @@ function Home() {
         
       </header>
 
-      <main className="text-center">
-        <h1>Hayatul Islamiya School Management System</h1>
-        <p className="fs-5 text-center">
-          Huu ni mfumo wa uendeshaji na utoaji wa taarifa mbalimbali zinazohusu shule na uendeshaji. Mfumo huu unatumika na walimu wa shule hii kupata na kuhifadhi taarifa za maendeleo ya shule na wanafunzi.
-           Pia ni kiunganisha kati ya shule na wazazi katika kutoa na kupata taarifa za maendeleo ya wanafunzi moja kwa moja.
-        </p>
+      <main>
+        <div className="text-center">
+          <h1>Hayatul Islamiya School Management System</h1>
+          <p className="fs-5 text-center">
+            Huu ni mfumo wa uendeshaji na utoaji wa taarifa mbalimbali zinazohusu shule na uendeshaji. Mfumo huu unatumika na walimu wa shule hii kupata na kuhifadhi taarifa za maendeleo ya shule na wanafunzi.
+            Pia ni kiunganisha kati ya shule na wazazi katika kutoa na kupata taarifa za maendeleo ya wanafunzi moja kwa moja.
+          </p>
 
-        <div className="mb-5">
-          <Link to="/login" className="btn btn-primary btn-lg px-4">Ingia</Link>
+          <div className="mb-5">
+            <Link to="/login" className="btn btn-primary btn-lg px-4">Ingia</Link>
+          </div>
         </div>
 
         <hr className="col-3 col-md-2 mb-5" />
 
         <div className="row g-5">
           <div className="col-md-6">
-            <h2>Namna ya kutumia mfumo huu</h2>
+            <h2 className="text-center">Namna ya kutumia mfumo huu</h2>
             <p>Soma maelekezo ya kutumia mfumo huu kulingana na aina ya mtumiaji.</p>
-            <ul className="icon-list">
-              <li><Link to="/getting-started/teachers">Walimu</Link></li>
-              <li><Link to="/getting-started/parents">Wazazi</Link></li>
-              <li><Link to="/getting-started/developers">Developers</Link></li>
+            <ul className="list-unstyled">
+              <li><Link to="/getting-started/teachers">Walimu</Link></li> <hr className='mb-0 mt-0' />
+              <li><Link to="/getting-started/parents">Wazazi</Link></li> <hr className='mb-0 mt-0' />
+              <li><Link to="/getting-started/developers">Developers</Link></li> <hr className='mb-0 mt-0' />
             </ul>
           </div>
 
           <div className="col-md-6">
-            <h2>Developers</h2>
-            <p>
+            <h2 className="text-center">Developers</h2>
+            <p className='text-left'>
               All developers are welcome to join in on this open Source project, This projects gives opportunity to all developers who can contribute anything, so all developers with JavaScript, React and Redux are wecome to join in and help on this project.
             </p>
-            <ul className="icon-list">
-              <li className=''><Link to="https://github.com/mwinamijr/react-scms">React School Management System</Link> Github repository</li>
+            <hr className='mb-0 mt-0' />
+            <ul className="list-unstyled">
+              <li className=''><Link to="https://github.com/mwinamijr/react-scms">React School Management System</Link> Github repository</li> <hr className='mb-0 mt-0' />
               <li className=''>
                 For instructions on how to contribute to this project check out the developers manual<Link to="/getting-started/developers"> here</Link>
-              </li>
+              </li> <hr className='mb-0 mt-0' />
             </ul>
           </div>
         </div>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { Form, Button, } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
@@ -30,41 +31,44 @@ function LoginScreen() {
         return <Navigate replace to="/" />
     } else {
         return (
+          <div>
+            <div className='container'>
+              <Link to="/" className='btn btn-light my-3'>Go Back</Link>
+            </div>
             <FormContainer>
-                <h1>Sign In</h1>
-                {error && <Message variant='danger'>{error}</Message>}
-                {loading && <Loader />}
-                <Form onSubmit={submitHandler}>
-    
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control
-                            type='email'
-                            placeholder='Enter Email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        >
-                        </Form.Control>
-                    </Form.Group>
-    
-    
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type='password'
-                            placeholder='Enter Password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        >
-                        </Form.Control>
-                    </Form.Group>
-    
-                    <Button type='submit' variant='primary'>
-                        Sign In
-                    </Button>
-                </Form>
-    
+              <h1>Sign In</h1>
+              {error && <Message variant='danger'>{error}</Message>}
+              {loading && <Loader />}
+              <Form onSubmit={submitHandler}>
+  
+                <Form.Group controlId='email'>
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                    type='email'
+                    placeholder='Enter Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Enter Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+                <br />
+                <Button type='submit' variant='primary'>
+                  Sign In
+                </Button>
+              </Form>
             </FormContainer>
+          </div>
         )
     }
 }
