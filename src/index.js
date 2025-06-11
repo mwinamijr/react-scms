273855573e7afc338048240e2baab2c23dfcc840
mwinamijr <mwinamijr@gmail.com>
2025-06-11 21:34:17 +0300

    removed unused assignment and student-related Redux files to streamline the codebase

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { Provider } from "react-redux";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
